c40980c69a328615be8cdc0a122f8b4d897ead71
Anatoly Antonenko <ya.tolik-a@yandex.ru>
2020-02-26 01:43:23 +0300

    26.02

---
+++ js/common.js
@@ -0,0 +1,16 @@
+$(function() {
+/*
+    $("td").click(function(){
+    if (!$(this).hasClass("disabled-day") && !$(this).hasClass("number-day") && !$(this).hasClass("cross-day")) {
+     var radioValue = $("input[name='day']:checked").val();
+     if(radioValue){
+       $(this).removeClass();
+       $(this).addClass(radioValue);
+     }
+    }
+    });
+*/
+
+});
+
+
